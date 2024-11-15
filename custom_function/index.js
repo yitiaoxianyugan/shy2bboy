e289d2e27d4df9dcd4c9d74d299a335402963aed
xja <xiaojunan@haomo-studio.com>
2024-11-15 17:48:06 +0800

    fix:完善启动页

--- custom_function/index.js
+++ custom_function/index.js
@@ -1,18 +1,38 @@
 import moment from "moment";
 
 import UserData from "../dataJson/user.json"
-
+const userList = UserData.table_list;
+// 格式化时间
 export function formatDate(date, format) {
 	format = format || "YYYY/MM/DD";
 	let newDate = moment(date).format(format)
 	return newDate;
 }
-export function searchUser(key,value){
-	console.log("UserData",UserData)
-	return id;
+// 查找用户
+export function searchUser(key, value) {
+	const user = userList.find(item => item[key] === value);
+	return user === undefined ? false : user;
 }
-export function login(userInfo){
-	return false
+// 登录
+export function login(userInfo) {
+	let tempItem = {
+		user_id: userInfo.account,
+		user_pwd: userInfo.password
+	}
+	const user = userList.find(item => item.user_id === tempItem.user_id);
+	if (user === undefined) {
+		showToast("用户不存在!");
+		return false;
+	} else {
+		if (tempItem.user_pwd === user.user_pwd) {
+			showToast("登录成功!");
+			uni.setStorageSync("user_info",user);
+			return true;
+		} else {
+			showToast("密码错误!");
+			return false;
+		}
+	}
 }
 // 展示提示
 export function showToast(title, type, duration) {
@@ -23,3 +43,24 @@ export function showToast(title, type, duration) {
 		duration: duration || 2000
 	});
 }
+
+// 加载状态
+let dots = ".";
+export function loadState(state,loadTextBase,dotNum){
+	let loadText = "";
+	loadTextBase = loadTextBase || "加载中";
+	dotNum = dotNum || 6;
+	if(state){
+		loadText = loadTextBase + dots
+		if(dots.length >= dotNum){
+			dots = ".";
+		}else{
+			dots += "."
+		}
+	}else{
+		dots = ".";
+		loadText = ""
+	}
+	return loadText;
+}
+
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,46 @@
 <template>
-	<view class="page-bg content d-flex flex-center">
+	<view class="page-bg content f-col flex-center gap-8">
 		<div class="image-box">
 			<image class="logo" src="/static/poster.png" style="width:100%;height:100%"></image>
+		</div>
+		<div class="loading-text">
+			{{loadText}}
 		</div>
 	</view>
 </template>
 
 <script>
-	import { searchUser } from "../../custom_function/index.js"
+	import {
+		searchUser,
+		loadState
+	} from "../../custom_function/index.js"
 	export default {
 		data() {
 			return {
-				loading: true
+				loading: true,
+				loadText: "加载中",
+				timer: null
 			}
 		},
 		onLoad() {
 			this.onloadUserinfo()
+		},
+		watch: {
+			loading: {
+				handler: function(newValue, value) {
+					let timer;
+					if (newValue) {
+						timer = setInterval(e => {
+							this.loadText = loadState(true)
+						}, 500)
+					} else {
+						this.loadText = loadState(false)
+						clearInterval(timer)
+					}
+				},
+				deep: true,
+				immediate: true
+			}
 		},
 		methods: {
 			onloadUserinfo() {
@@ -24,16 +49,15 @@
 				uni.getStorage({
 					key: 'user_info',
 					complete: function(res) {
-						console.log("complete", res.data.id);
-						if(res.data && res.data.id){
-							const data = searchUser(res.data.id);
-							uni.setStorageSync('user_info',data)
+						if (res.data && res.data.id) {
+							const data = searchUser("id", res.data.id);
+							uni.setStorageSync('user_info', data)
 							uni.navigateTo({
-								url:"/pages/home/index"
+								url: "/pages/home/index"
 							})
-						}else{
+						} else {
 							uni.navigateTo({
-								url:"/pages/login/index"
+								url: "/pages/login/index"
 							})
 						}
 						self.loading = false;
@@ -43,17 +67,24 @@
 		},
 		beforeDestroy() {
 			this.loading = false;
+			clearInterval(this.timer);
 		}
 	}
 </script>
 
 <style lang="less">
 	.content {
+		position: relative;
+
 		.image-box {
 			width: 61.8%;
 			aspect-ratio: 1/1;
 			border-radius: .875rem;
 			overflow: hidden;
+		}
+
+		.loading-text {
+			color: #1890ff;
 		}
 	}
 </style>
